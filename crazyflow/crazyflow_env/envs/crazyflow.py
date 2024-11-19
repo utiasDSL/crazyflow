9734c8a6fbfab48ca5ab7a214b4d130a19c628f3
OliEfr <ollihaus@t-online.de>
2024-11-19 11:40:16 +0100

    WIP: gym env

--- crazyflow/crazyflow_env/envs/crazyflow.py
+++ crazyflow/crazyflow_env/envs/crazyflow.py
@@ -1,8 +1,9 @@
 from enum import Enum
-from typing import Optional, Tuple, Union
+from typing import Optional, Tuple, Union, List
 import gymnasium as gym
 from gymnasium import spaces
 import numpy as np
+import math
 
 from gymnasium.vector import VectorEnv
 from gymnasium.vector.utils import batch_space
@@ -24,10 +25,10 @@ class CrazyflowVectorEnv(VectorEnv):
         num_drones_per_env: int = 1,
         max_episode_steps: int = 1000,
         render_mode: Optional[str] = None,
-
-        # Sim parameters, TODO: pass as kwargs
+        # Sim parameters
+        # TODO: pass as kwargs
+        control: Control = Control.default,
         physics=Physics.analytical,
-        control=Control.state,
         controller=Controller.emulatefirmware,
         freq=500,
         control_freq=500,
@@ -38,11 +39,18 @@ class CrazyflowVectorEnv(VectorEnv):
         self.num_drones_per_env = num_drones_per_env
         self.max_episode_steps = max_episode_steps
         self.render_mode = render_mode
+        
+        self.control = control
+        self.controller = controller
+        self.freq = freq
+        self.control_freq = control_freq
+        self.device = device
 
         # Init physics sim
         self.sim = Sim(
             n_worlds=num_envs,
             n_drones=num_drones_per_env,
+            # TODO pass as kwargs
             physics=physics,
             control=control,
             controller=controller,
@@ -51,34 +59,30 @@ class CrazyflowVectorEnv(VectorEnv):
             device=device,
         )
 
-        # More env parameters
-        self.steps = np.zeros(num_envs, dtype=np.int32)
-        self.prev_done = np.zeros(num_envs, dtype=np.bool_)
+        # Env setup
+        self.steps = np.zeros((num_envs, num_drones_per_env), dtype=np.int32)
+        self.prev_done = np.zeros((num_envs, num_drones_per_env), dtype=np.bool_)
 
-        # TODO: set observation_space and action_space
-        self.single_action_space = spaces.Discrete(2)
+        # TODO: scale action spaces
+        self.single_action_space = spaces.Box(
+            -1, 1, shape=(math.prod(self.sim._controls[control].shape[1:]),), dtype=np.float32
+        ) # num_drones_per_env * num_controls
         self.action_space = batch_space(self.single_action_space, num_envs)
-        self.single_observation_space = spaces.Box(-np.inf, np.inf, dtype=np.float32)
+
+        _obs_size = 0
+        for key in self.sim.states.keys():
+            _obs_size += math.prod(self.sim.states[key].shape[1:]) # num_obs_per_drone * num_controls
+        self.single_observation_space = spaces.Box(-np.inf, np.inf, shape=(_obs_size,), dtype=np.float32) # set limits to np.inf as we are expecting a normalization wrapper
         self.observation_space = batch_space(self.single_observation_space, num_envs)
-        
-        self.state = None
-        self.steps_beyond_terminated = None
 
         def step(
             self, action: np.ndarray
         ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, dict]:
-            assert self.action_space.contains(
-                action
-            ), f"{action!r} ({type(action)}) invalid"
+            assert self.action_space.contains(action), f"{action!r} ({type(action)}) invalid"
             assert self.state is not None, "Call reset before using step method."
             pass
 
-        def reset(
-            self,
-            *,
-            seed: Optional[int] = None,
-            options: Optional[dict] = None,
-        ):
+        def reset(self, *, seed: Optional[int] = None, options: Optional[dict] = None):
             # Resets all (!) envs
             super().reset(seed=seed)
             pass
--- examples/gymnasium_env.py
+++ examples/gymnasium_env.py
@@ -1,4 +1,19 @@
 import gymnasium
 import crazyflow.crazyflow_env.envs
 
-env = gymnasium.make_vec("crazyflow_env/CrazyflowVectorEnv-v0")
+# TODO pass additional parameters for setting up the sim
+envs = gymnasium.make_vec("crazyflow_env/CrazyflowVectorEnv-v0", num_envs=4, num_drones_per_env=3)
+
+obs, info = envs.reset(seed=42)
+_ = envs.action_space.seed(42)
+
+# Step through the environment with randomly sampled actions
+for _ in range(10_000):
+    action = envs.action_space.sample()
+    observation, reward, terminated, truncated, info = envs.step(action)
+    # TODO: Add your code here to process the observation, reward, done, and info variables
+    if terminated or truncated:
+        print("Episode terminated")
+        break
+
+envs.close()
