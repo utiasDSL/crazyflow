0af6d1ce301b239ad3ad2073f11fb0898a778f96
OliEfr <ollihaus@t-online.de>
2024-11-20 12:25:21 +0100

    improve gym env

--- crazyflow/crazyflow_env/envs/crazyflow.py
+++ crazyflow/crazyflow_env/envs/crazyflow.py
@@ -1,9 +1,9 @@
-from enum import Enum
 from typing import Optional, Tuple, Union, List
 import gymnasium as gym
 from gymnasium import spaces
 import numpy as np
 import math
+import jax
 
 from gymnasium.vector import VectorEnv
 from gymnasium.vector.utils import batch_space
@@ -15,77 +15,106 @@ from crazyflow.sim.physics import Physics
 
 # Example for Env and VectorEnv here: https://github.com/Farama-Foundation/Gymnasium/blob/main/gymnasium/envs/classic_control/cartpole.py#L476
 class CrazyflowVectorEnv(VectorEnv):
-    # TODO dynamically set render_fps
-    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 60}
 
     def __init__(
         self,
-        # Env parameters
         num_envs: int = 1,
         num_drones_per_env: int = 1,
         max_episode_steps: int = 1000,
-        render_mode: Optional[str] = None,
-        # Sim parameters
-        # TODO: pass as kwargs
-        control: Control = Control.default,
-        physics=Physics.analytical,
-        controller=Controller.emulatefirmware,
-        freq=500,
-        control_freq=500,
-        device="cpu",
+        **kwargs: dict,
     ):
-        # Env parameters
+
         self.num_envs = num_envs
         self.num_drones_per_env = num_drones_per_env
         self.max_episode_steps = max_episode_steps
-        self.render_mode = render_mode
-        
-        self.control = control
-        self.controller = controller
-        self.freq = freq
-        self.control_freq = control_freq
-        self.device = device
 
-        # Init physics sim
         self.sim = Sim(
             n_worlds=num_envs,
             n_drones=num_drones_per_env,
-            # TODO pass as kwargs
-            physics=physics,
-            control=control,
-            controller=controller,
-            freq=freq,
-            control_freq=control_freq,
-            device=device,
+            **kwargs,
         )
 
-        # Env setup
-        self.steps = np.zeros((num_envs, num_drones_per_env), dtype=np.int32)
-        self.prev_done = np.zeros((num_envs, num_drones_per_env), dtype=np.bool_)
+        self.steps = np.zeros((num_envs), dtype=np.int32)
+        self.prev_done = np.zeros((num_envs), dtype=np.bool_)
 
-        # TODO: scale action spaces
         self.single_action_space = spaces.Box(
-            -1, 1, shape=(math.prod(self.sim._controls[control].shape[1:]),), dtype=np.float32
-        ) # num_drones_per_env * num_controls
+            -1, 1, shape=(math.prod(self.sim._controls[self.sim.control].shape[1:]),), dtype=np.float32
+        )  # num_drones_per_env * num_controls
         self.action_space = batch_space(self.single_action_space, num_envs)
 
         _obs_size = 0
         for key in self.sim.states.keys():
-            _obs_size += math.prod(self.sim.states[key].shape[1:]) # num_obs_per_drone * num_controls
-        self.single_observation_space = spaces.Box(-np.inf, np.inf, shape=(_obs_size,), dtype=np.float32) # set limits to np.inf as we are expecting a normalization wrapper
+            _obs_size += math.prod(
+                self.sim.states[key].shape[1:]
+            )  # num_obs_per_drone * num_controls
+        self.single_observation_space = spaces.Box(
+            -np.inf, np.inf, shape=(_obs_size,), dtype=np.float32
+        )  # set limits to np.inf as we are expecting a normalization wrapper
         self.observation_space = batch_space(self.single_observation_space, num_envs)
 
-        def step(
-            self, action: np.ndarray
-        ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, dict]:
-            assert self.action_space.contains(action), f"{action!r} ({type(action)}) invalid"
-            assert self.state is not None, "Call reset before using step method."
-            pass
+    def step(
+        self, action: np.ndarray
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, dict]:
+        assert self.action_space.contains(action), f"{action!r} ({type(action)}) invalid"
+        action = action.reshape((self.num_envs, self.num_drones_per_env, -1))
 
-        def reset(self, *, seed: Optional[int] = None, options: Optional[dict] = None):
-            # Resets all (!) envs
-            super().reset(seed=seed)
-            pass
+        # TODO: rescale actions
+        if self.sim.control == Control.state:
+            # TODO Do I need to check self.sim.controllable() ?
+            self.sim.state_control(action)
+        elif self.sim.control == Control.attitude:
+            self.sim.attitude_control(action)
+        elif self.sim.control == Control.thrust:
+            self.sim.thrust_control(action)
+        else:
+            raise ValueError(f"Invalid control type {self.sim.control}")
 
-        def render(self):
-            pass
+        self.sim.step()
+
+        terminated = self._get_terminated()
+
+        reward = self._get_reward()
+
+        self.steps += 1
+
+        truncated = self.steps >= self.max_episode_steps
+
+        # TODO Reset all environments which terminated or were truncated in the last step
+
+        self.steps[self.prev_done] = 0
+        reward[self.prev_done] = 0.0
+        terminated[self.prev_done] = False
+        truncated[self.prev_done] = False
+
+        self.prev_done = np.logical_or(terminated, truncated)
+
+        return self._get_obs(), reward, terminated, truncated, {}
+
+    def reset(self, *, seed: Optional[int] = None, options: Optional[dict] = None):
+        # Resets all (!) envs
+        super().reset(seed=seed)
+        self.sim.reset()
+
+        if "pos" in options:
+            self.sim.states["pos"] = options["pos"]
+
+        self.steps = np.zeros(self.num_envs, dtype=np.int32)
+        self.prev_done = np.zeros(self.num_envs, dtype=np.bool_)
+
+        return self._get_obs(), {}
+    
+   
+    def _get_obs(self) -> np.ndarray:
+        # Returns observations for all envs in a dictionary. Each entry is of shape (num_envs, num_drones_per_env * num_obs_per_drone)
+        return {key: jax.device_get(value.reshape(*value.shape[:-2], -1)) for key, value in self.sim.states.items()}
+    
+    def _get_reward(self) -> np.ndarray:
+        # Returns rewards for all envs
+        return np.zeros((self.num_envs), dtype=np.float32)
+
+    def _get_terminated(self) -> np.ndarray:
+        # Returns termination status for all envs
+        return np.zeros((self.num_envs), dtype=np.bool_)
+
+    def render(self):
+        self.sim.render()
--- examples/gymnasium_env.py
+++ examples/gymnasium_env.py
@@ -1,19 +1,51 @@
 import gymnasium
+import numpy as np
 import crazyflow.crazyflow_env.envs
+from ml_collections import config_dict
 
-# TODO pass additional parameters for setting up the sim
-envs = gymnasium.make_vec("crazyflow_env/CrazyflowVectorEnv-v0", num_envs=4, num_drones_per_env=3)
+from crazyflow.control.controller import Control, Controller
+from crazyflow.sim.core import Sim
+from crazyflow.sim.physics import Physics
 
-obs, info = envs.reset(seed=42)
-_ = envs.action_space.seed(42)
 
-# Step through the environment with randomly sampled actions
-for _ in range(10_000):
-    action = envs.action_space.sample()
+num_envs = 2
+num_drones_per_env = 10
+
+# set config for simulation
+sim_config = config_dict.ConfigDict()
+sim_config.device = "cpu"
+sim_config.physics = Physics.sys_id
+sim_config.control = Control.default
+sim_config.controller = Controller.default
+sim_config.freq = 60
+
+envs = gymnasium.make_vec(
+    "crazyflow_env/CrazyflowVectorEnv-v0",
+    num_envs=num_envs,
+    num_drones_per_env=num_drones_per_env,
+    max_episode_steps=1000,
+    **sim_config,
+)
+
+# determine init positions of drones as a grid
+N = int(np.ceil(np.sqrt(num_drones_per_env)))
+points = np.linspace(-0.5 * (N - 1), 0.5 * (N - 1), N)
+x, y = np.meshgrid(points, points)
+grid = np.stack((x.flatten(), y.flatten(), np.zeros_like(y).flatten()), axis=-1)
+grid = grid[:num_drones_per_env]
+
+# action for going up (in attitude control)
+action = np.array(
+    [[[0.3, 0, 0, 0] for _ in range(num_drones_per_env)] for _ in range(num_envs)], dtype=np.float32
+).reshape(num_envs, -1)
+
+obs, info = envs.reset(seed=42, options={"pos": grid})
+
+# Step through the environment
+for _ in range(1500):
     observation, reward, terminated, truncated, info = envs.step(action)
-    # TODO: Add your code here to process the observation, reward, done, and info variables
-    if terminated or truncated:
-        print("Episode terminated")
-        break
+    envs.render()
+    if any(terminated) or any(truncated):
+        envs.reset(options={"pos": grid})
 
 envs.close()
