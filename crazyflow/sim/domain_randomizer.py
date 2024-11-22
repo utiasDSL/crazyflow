b91c2c751e5cbf78ec9a41f12b0f6a1c998f041f
middleyuan <alex730310@gmail.com>
2024-11-22 10:40:06 +0100

    initial implementation for domain randomization.

--- crazyflow/sim/domain_randomizer.py
+++ crazyflow/sim/domain_randomizer.py
@@ -4,7 +4,7 @@ import mujoco.mjx as mjx
 
 class DomainRandomizer:
     def __init__(self, env_num: int, seed: int):
-        """Initialize the domain randomizer with model and seed.
+        """Initialize the domain randomizer with seed.
 
         Parameters:
             env_num (int): Number of environments.
@@ -12,7 +12,7 @@ class DomainRandomizer:
         """
         self.rng = jax.random.PRNGKey(seed)
         self.rng = jax.random.split(self.rng, env_num)
-
+    
     def randomize_friction_from_distribution(
         self, sys: mjx.Model, distirbution: str, friction_range: tuple
     ) -> mjx.Model:
@@ -33,7 +33,6 @@ class DomainRandomizer:
 
             Parameters:
                 key: Random key.
-                friction_range: The range of friction coefficient.
 
             Returns:
                 res_sys: The randomized system
@@ -45,5 +44,162 @@ class DomainRandomizer:
             return res_sys
 
         batched_sys = jax.vmap(_randomize_friction)(self.rng)
+
+        return batched_sys
+    
+    def randomize_friction_from_values(
+        self, sys: mjx.Model, friction_values: jax.Array
+    ) -> mjx.Model:
+        """Randomize the friction coefficient of the system from a list of values.
+
+        Parameters:
+            sys: The system model.
+            friction_values: The list of friction coefficient values.
+
+        Returns:
+            batched_sys: The batched randomized system.
+        """
+        def _randomize_friction(friction: float) -> mjx.Model:
+            """Randomize the friction coefficient of the system from a list of values.
+
+            Parameters:
+                friction: Friction coefficient value.
+
+            Returns:
+                res_sys: The randomized system
+            """
+            friction = sys.geom_friction.at[:, 0].set(friction)
+            res_sys = sys.tree_replace({"geom_friction": friction})
+            return res_sys
+        
+        batched_sys = jax.vmap(_randomize_friction)(friction_values)
+
+        return batched_sys
+    
+    def randomize_mass_from_distribution(
+        self, sys: mjx.Model, distirbution: str, mass_range: tuple
+    ) -> mjx.Model:
+        """Randomize the mass of the system from a uniform distribution.
+
+        Parameters:
+            sys: The system model.
+            distribution: The distribution to sample from.
+            mass_range: The range of mass.
+
+        Returns:
+            batched_sys: The batched randomized system.
+        """
+        assert distirbution == "uniform", "Only uniform distribution is supported."
+
+        def _randomize_mass(key: jax.random.PRNGKey) -> mjx.Model:
+            """Randomize the mass of the system from a uniform distribution.
+
+            Parameters:
+                key: Random key.
+
+            Returns:
+                res_sys: The randomized system
+            """
+            minval, maxval = mass_range
+            mass = jax.random.uniform(key, shape=(), minval=minval, maxval=maxval)
+            mass = sys.body_mass.at[1].set(mass)
+            res_sys = sys.tree_replace({"body_mass": mass})
+            return res_sys
+
+        batched_sys = jax.vmap(_randomize_mass)(self.rng)
+
+        return batched_sys
+    
+    def randomize_mass_from_values(
+        self, sys: mjx.Model, mass_values: jax.Array
+    ) -> mjx.Model:
+        """Randomize the mass of the system from a list of values.
+
+        Parameters:
+            sys: The system model.
+            mass_values: The list of mass values.
+
+        Returns:
+            batched_sys: The batched randomized system.
+        """
+        def _randomize_mass(mass: float) -> mjx.Model:
+            """Randomize the mass of the system from a list of values.
+
+            Parameters:
+                mass: Mass value.
+
+            Returns:
+                res_sys: The randomized system
+            """
+            mass = sys.body_mass.at[1].set(mass)
+            res_sys = sys.tree_replace({"body_mass": mass})
+            return res_sys
+        
+        batched_sys = jax.vmap(_randomize_mass)(mass_values)
+
+        return batched_sys
+    
+    def randomize_body_positions_from_distribution(
+        self, sys: mjx.Model, distirbution: str, perturb_range: tuple
+    ) -> mjx.Model:
+        """Randomize positions of bodies in the system from a uniform distribution.
+
+        Parameters:
+            sys: The system model.
+            distribution: The distribution to sample from.
+            mass_range: The range of mass.
+
+        Returns:
+            batched_sys: The batched randomized system.
+        """
+        assert distirbution == "uniform", "Only uniform distribution is supported."
+
+        def _perturb_positions(key: jax.random.PRNGKey) -> mjx.Model:
+            """Perturb the positions of the bodies in the system.
+
+            Parameters:
+                key: Random key.
+
+            Returns:
+                res_sys: The randomized system.
+            """
+            min_offset, max_offset = perturb_range
+            perturbations = jax.random.uniform(key, shape=sys.body_pos.shape, minval=min_offset, maxval=max_offset)
+            new_body_pos = sys.body_pos + perturbations
+            res_sys = sys.tree_replace({"body_pos": new_body_pos})
+            return res_sys
+
+        # Apply perturbation across batched keys
+        batched_sys = jax.vmap(_perturb_positions)(self.rng)
+
+        return batched_sys
+    
+    def randomize_body_positions_from_values(
+        self, sys: mjx.Model, perturb_values: jax.Array
+    ) -> mjx.Model:
+        """Randomize positions of bodies in the system from a list of values.
+
+        Parameters:
+            sys: The system model.
+            perturb_values: The list of perturbation values.
+
+        Returns:
+            batched_sys: The batched randomized system.
+        """
+        def _perturb_positions(perturb: jax.Array) -> mjx.Model:
+            """Perturb the positions of the bodies in the system.
+
+            Parameters:
+                perturb: Perturbation values.
+
+            Returns:
+                res_sys: The randomized system.
+            """
+            new_body_pos = sys.body_pos + perturb
+            res_sys = sys.tree_replace({"body_pos": new_body_pos})
+            return res_sys
+
+        # Apply perturbation across batched values
+        batched_sys = jax.vmap(_perturb_positions)(perturb_values)
 
         return batched_sys
--- examples/domain_randomization.py
+++ examples/domain_randomization.py
@@ -1,19 +1,29 @@
-import numpy as np
+import jax
 
 from crazyflow.sim import Physics, Sim
 from crazyflow.sim.domain_randomizer import DomainRandomizer
 
 
 def main():
-    """Spawn multiple drones with domain randomization."""
+    """Spawn multiple envs with domain randomization."""
     n_worlds, n_drones = 1, 1
     sim = Sim(n_worlds=n_worlds, n_drones=n_drones, physics=Physics.sys_id, device="cpu")
-    fps = 60
 
-    # Randomize friciton
+    example_1(sim)
+    example_2(sim)
+    example_3(sim)
+    example_4(sim)
+    example_5(sim)
+    example_6(sim)
+
+    sim.close()
+
+def example_1(sim):
+    """Randomize friciton from a uniform distribution. """
     env_num = 10
+    friction_range = (0.6, 1.4)
     domain_randomizer = DomainRandomizer(env_num=env_num, seed=0)
-    batched_sys = domain_randomizer.randomize_friction_from_distribution(sim._mjx_model, "uniform", (0.6, 1.4))
+    batched_sys = domain_randomizer.randomize_friction_from_distribution(sim._mjx_model, "uniform", friction_range)
 
     print('Single env friction shape: ', sim._mjx_model.geom_friction.shape)
     print('Batched env friction shape: ', batched_sys.geom_friction.shape)
@@ -21,8 +31,85 @@ def main():
     print('Friction on geom 0: ', sim._mjx_model.geom_friction[0, 0])
     print('Random frictions on geom 0: ', batched_sys.geom_friction[:, 0, 0])
 
-    sim.close()
+def example_2(sim):
+    """Randomize friction from a list of values."""
+    env_num = 10
+    friction_range = (0.6, 1.4)
+    domain_randomizer = DomainRandomizer(env_num=env_num, seed=0)
 
+    def helper(key):
+        return jax.random.uniform(key, shape=(1,), minval=friction_range[0], maxval=friction_range[1])
+    
+    frictions = jax.vmap(helper)(domain_randomizer.rng)
+    batched_sys = domain_randomizer.randomize_friction_from_values(sim._mjx_model, frictions)
+
+    print('Single env friction shape: ', sim._mjx_model.geom_friction.shape)
+    print('Batched env friction shape: ', batched_sys.geom_friction.shape)
+
+    print('Friction on geom 0: ', sim._mjx_model.geom_friction[0, 0])
+    print('Random frictions on geom 0: ', batched_sys.geom_friction[:, 0, 0])
+
+def example_3(sim):
+    """Randomize mass from a uniform distribution."""
+    env_num = 10
+    mass_range = (0.022, 0.032)
+    domain_randomizer = DomainRandomizer(env_num=env_num, seed=0)
+    batched_sys = domain_randomizer.randomize_mass_from_distribution(sim._mjx_model, "uniform", mass_range)
+
+    print('Single env mass shape: ', sim._mjx_model.body_mass.shape)
+    print('Batched env mass shape: ', batched_sys.body_mass.shape)
+
+    print('Mass on part 1: ', sim._mjx_model.body_mass[1])
+    print('Random mass on part 1: ', batched_sys.body_mass[:, 1])
+
+def example_4(sim):
+    """Randomize mass from a list."""
+    env_num = 10
+    mass_range = (0.022, 0.032)
+    domain_randomizer = DomainRandomizer(env_num=env_num, seed=0)
+
+    def helper(key):
+        return jax.random.uniform(key, shape=(), minval=mass_range[0], maxval=mass_range[1])
+    
+    masses = jax.vmap(helper)(domain_randomizer.rng)
+    batched_sys = domain_randomizer.randomize_mass_from_values(sim._mjx_model, masses)
+
+    print('Single env mass shape: ', sim._mjx_model.body_mass.shape)
+    print('Batched env mass shape: ', batched_sys.body_mass.shape)
+
+    print('Mass on part 1: ', sim._mjx_model.body_mass[1])
+    print('Random mass on part 1: ', batched_sys.body_mass[:, 1])
+
+def example_5(sim):
+    """Randomize the position of the motors from a uniform distribution."""
+    env_num = 10
+    position_range = (-0.02, 0.02)
+    domain_randomizer = DomainRandomizer(env_num=env_num, seed=0)
+    batched_sys = domain_randomizer.randomize_body_positions_from_distribution(sim._mjx_model, "uniform", position_range)
+
+    print('Single env motor position shape: ', sim._mjx_model.body_pos.shape)
+    print('Batched env motor position shape: ', batched_sys.body_pos.shape)
+
+    print('Motor positions: ', sim._mjx_model.body_pos)
+    print('Random motor positions: ', batched_sys.body_pos)
+
+def example_6(sim):
+    """Randomize the position of the motors from a list of values."""
+    env_num = 10
+    position_range = (-0.02, 0.02)
+    domain_randomizer = DomainRandomizer(env_num=env_num, seed=0)
+
+    def helper(key):
+        return jax.random.uniform(key, shape=(3,), minval=position_range[0], maxval=position_range[1])
+    
+    positions = jax.vmap(helper)(domain_randomizer.rng)
+    batched_sys = domain_randomizer.randomize_body_positions_from_values(sim._mjx_model, positions)
+
+    print('Single env motor position shape: ', sim._mjx_model.body_pos.shape)
+    print('Batched env motor position shape: ', batched_sys.body_pos.shape)
+
+    print('Motor positions: ', sim._mjx_model.body_pos)
+    print('Random motor positions: ', batched_sys.body_pos)
 
 if __name__ == "__main__":
     main()
