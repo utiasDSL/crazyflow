Add thrust interface. Remove gym env scaling. Fix action and observation spaces

benchmark/main.py
@@ -52,8 +52,7 @@ def profile_gym_env_step(sim_config: config_dict.ConfigDict, n_steps: int, devic
 
     # Action for going up (in attitude control)
     action = np.zeros((sim_config.n_worlds, 4), dtype=np.float32)
-    action[..., 0] = -0.3
-
+    action[..., 0] = 0.3
     # Step through env once to ensure JIT compilation
     envs.reset_all(seed=42)
     envs.step(action)

crazyflow/control/controller.py
@@ -106,12 +106,13 @@ def attitude2rpm(
     # PID target torques.
     target_torques = -P_T * rot_e + D_T * rpy_rates_e + I_T * rpy_err_i
     target_torques = jnp.clip(target_torques, -3200, 3200)
-    pwm = jnp.clip(collective_thrust2pwm(cmd[0]) + MIX_MATRIX @ target_torques, MIN_PWM, MAX_PWM)
-    return PWM2RPM_CONST + PWM2RPM_SCALE * pwm, rpy_err_i
+    thrust_per_motor = cmd[0] / 4
+    pwm = jnp.clip(thrust2pwm(thrust_per_motor) + MIX_MATRIX @ target_torques, MIN_PWM, MAX_PWM)
+    return pwm2rpm(pwm), rpy_err_i
 
 
-def collective_thrust2pwm(thrust: Array) -> Array:
-    """Convert the desired collective thrust into motor PWMs.
+def thrust2pwm(thrust: Array) -> Array:
+    """Convert the desired thrust into motor PWM.
 
     Args:
         thrust: The desired thrust per motor.
@@ -119,4 +120,10 @@ def collective_thrust2pwm(thrust: Array) -> Array:
     Returns:
         The motors' PWMs to apply to the quadrotor.
     """
-    return jnp.clip((jnp.sqrt(thrust / (KF * 4)) - PWM2RPM_CONST) / PWM2RPM_SCALE, MIN_PWM, MAX_PWM)
+    thrust = jnp.clip(thrust, MIN_THRUST, MAX_THRUST)  # Protect against NaN values
+    return jnp.clip((jnp.sqrt(thrust / KF) - PWM2RPM_CONST) / PWM2RPM_SCALE, MIN_PWM, MAX_PWM)
+
+
+def pwm2rpm(pwm: Array) -> Array:
+    """Convert the motors' PWMs into RPMs."""
+    return PWM2RPM_CONST + PWM2RPM_SCALE * pwm

crazyflow/gymnasium_envs/crazyflow.py
@@ -1,11 +1,10 @@
-import math
 import warnings
 from functools import partial
 from typing import Literal
 
 import jax
 import jax.numpy as jnp
-from flax.struct import dataclass
+import numpy as np
 from gymnasium import spaces
 from gymnasium.vector import VectorEnv
 from gymnasium.vector.utils import batch_space
@@ -17,42 +16,56 @@ from crazyflow.sim.core import Sim
 from crazyflow.sim.structs import SimState
 
 
-@dataclass
-class RescaleParams:
-    scale_factor: Array
-    mean: Array
-
-
-CONTROL_RESCALE_PARAMS = {
-    "state": None,
-    "thrust": None,
-    "attitude": RescaleParams(
-        scale_factor=jnp.array(
-            [4 * (MAX_THRUST - MIN_THRUST) / 2, jnp.pi / 6, jnp.pi / 6, jnp.pi / 6]
-        ),
-        mean=jnp.array([4 * (MIN_THRUST + MAX_THRUST) / 2, 0.0, 0.0, 0.0]),
-    ),
-}
-
-
 @partial(jax.jit, static_argnames=["convert"])
 def maybe_to_numpy(data: Array, convert: bool) -> NDArray | Array:
     """Converts data to numpy array if convert is True."""
     return jax.lax.cond(convert, lambda: jax.device_get(data), lambda: data)
 
 
+def action_space(control_type: Control) -> spaces.Box:
+    """Select the appropriate action space for a given control type.
+
+    Args:
+        control_type: The desired control mode.
+
+    Returns:
+        The action space.
+    """
+    match control_type:
+        case Control.attitude:
+            return spaces.Box(
+                np.array([4 * MIN_THRUST, -np.pi, -np.pi, -np.pi], dtype=np.float32),
+                np.array([4 * MAX_THRUST, np.pi, np.pi, np.pi], dtype=np.float32),
+            )
+        case Control.thrust:
+            return spaces.Box(MIN_THRUST, MAX_THRUST, shape=(4,))
+        case _:
+            raise ValueError(f"Invalid control type {control_type}")
+
+
 class CrazyflowBaseEnv(VectorEnv):
-    """JAX Gymnasium environment for Crazyflie simulation."""
+    """JAX Gymnasium environment for Crazyflie simulation.
+
+    ## Action space
+    We have three types of actions:
+    - `attitude`: 4D vector consisting of [collective thrust, roll, pitch, yaw]
+    - `thrust`: 4D vector consisting of the individual motor thrusts [f1, f2, f3, f4]
+    - `state`: Currently not implemented
+
+    The default action space is `attitude`.
+    """
+
+    obs_keys = ["pos", "quat", "vel", "rpy_rates"]
 
     def __init__(
         self,
         *,
         num_envs: int = 1,  # required for VectorEnv
-        time_horizon_in_seconds: int = 10,
+        time_horizon_in_seconds: float = 10.0,
         return_datatype: Literal["numpy", "jax"] = "jax",
         **kwargs: dict,
     ):
-        """Summary: Initializes the CrazyflowEnv.
+        """Initialize the CrazyflowEnv.
 
         Args:
             num_envs: The number of environments to run in parallel.
@@ -63,70 +76,42 @@ class CrazyflowBaseEnv(VectorEnv):
             **kwargs: Takes arguments that are passed to the Crazyfly simulation.
         """
         assert num_envs == kwargs["n_worlds"], "num_envs must be equal to n_worlds"
-
         # Set random initial seed for JAX. For seeding, people should use the reset function
-        jax_seed = int(self.np_random.random() * 2**32)
-        self.jax_key = jax.random.key(jax_seed)
+        self.jax_key = jax.random.key(int(self.np_random.random() * 2**32))
 
         self.num_envs = num_envs
         self.return_datatype = return_datatype
         self.device = jax.devices(kwargs["device"])[0]
-        self.time_horizon_in_seconds = jnp.array(
-            time_horizon_in_seconds, dtype=jnp.int32, device=self.device
-        )
-
+        self.time_horizon_in_seconds = time_horizon_in_seconds
         self.sim = Sim(**kwargs)
 
-        assert (
-            self.sim.freq >= self.sim.control_freq
-        ), "Simulation frequency must be higher than control frequency"
+        assert self.sim.freq >= self.sim.control_freq, "Sim freq must be higher than control freq"
         if not self.sim.freq % self.sim.control_freq == 0:
-            warnings.warn(
-                "Simulation frequency should be a multiple of control frequency. We can handle the other case, but we highly recommend to change the simulation frequency to a multiple of the control frequency."
-            )
+            # We can handle other cases, but it's not recommended
+            warnings.warn("Simulation frequency should be a multiple of control frequency.")
+        if self.sim.control == Control.state:
+            raise NotImplementedError("State control currently not supported")
 
         self.n_substeps = self.sim.freq // self.sim.control_freq
-
         self.prev_done = jnp.zeros((self.sim.n_worlds), dtype=jnp.bool_, device=self.device)
 
-        self.single_action_space = spaces.Box(
-            -1,
-            1,
-            shape=(math.prod(getattr(self.sim.controls, self.sim.control).shape[1:]),),
-            dtype=jnp.float32,
-        )
+        self.single_action_space = action_space(self.sim.control)
         self.action_space = batch_space(self.single_action_space, self.sim.n_worlds)
 
-        self.states_to_include_in_obs = ["pos", "quat", "vel", "ang_vel", "rpy_rates"]
-        self._obs_size = 0
-        for state in self.states_to_include_in_obs:
-            if state == "pos":
-                self._obs_size += math.prod(
-                    getattr(self.sim.states, state)[0, :, 2].shape
-                )  # exclude x-y coordinates
-            else:
-                self._obs_size += math.prod(getattr(self.sim.states, state).shape[1:])
-        self.single_observation_space = spaces.Box(
-            -jnp.inf, jnp.inf, shape=(self._obs_size,), dtype=jnp.float32
+        self.single_observation_space = spaces.Dict(
+            {
+                "pos": spaces.Box(-np.inf, np.inf, shape=(3,)),
+                "quat": spaces.Box(-np.inf, np.inf, shape=(4,)),
+                "vel": spaces.Box(-np.inf, np.inf, shape=(3,)),
+                "rpy_rates": spaces.Box(-np.inf, np.inf, shape=(3,)),
+            }
         )
         self.observation_space = batch_space(self.single_observation_space, self.sim.n_worlds)
 
     def step(self, action: Array) -> tuple[Array, Array, Array, Array, dict]:
         assert self.action_space.contains(action), f"{action!r} ({type(action)}) invalid"
         action = self._sanitize_action(action, self.sim.n_worlds, self.sim.n_drones, self.device)
-        action = self._rescale_action(action, self.sim.control)
-
-        match self.sim.control:
-            case Control.state:
-                raise NotImplementedError(
-                    "Possibly you want to control state differences instead of absolute states"
-                )
-            case Control.attitude:
-                self.sim.attitude_control(action)
-            case Control.thrust:
-                self.sim.thrust_control(action)
-            case _:
-                raise ValueError(f"Invalid control type {self.sim.control}")
+        self._apply_action(action)
 
         for _ in range(self.n_substeps):
             self.sim.step()
@@ -146,29 +131,21 @@ class CrazyflowBaseEnv(VectorEnv):
         truncated = maybe_to_numpy(truncated, convert)
         return self._obs(), reward, terminated, truncated, {}
 
+    def _apply_action(self, action: Array):
+        match self.sim.control:
+            case Control.state:
+                raise NotImplementedError("State control currently not supported")
+            case Control.attitude:
+                self.sim.attitude_control(action)
+            case Control.thrust:
+                self.sim.thrust_control(action)
+            case _:
+                raise ValueError(f"Invalid control type {self.sim.control}")
+
     @staticmethod
     @partial(jax.jit, static_argnames=["n_worlds", "n_drones", "device"])
     def _sanitize_action(action: Array, n_worlds: int, n_drones: int, device: str) -> Array:
         return jnp.array(action, device=device).reshape((n_worlds, n_drones, -1))
-
-    @staticmethod
-    @partial(jax.jit, static_argnames=["control_type"])
-    def _rescale_action(action: Array, control_type: str) -> Array:
-        """Rescales actions based on the control type.
-
-        Args:
-            action (Array): Input actions to rescale.
-            control_type (str): The type of control (`state`, `attitude`, or `thrust`).
-
-        Returns:
-            Array: Rescaled actions.
-        """
-        params = CONTROL_RESCALE_PARAMS.get(control_type)
-        if params is None:
-            raise NotImplementedError(
-                f"Rescaling not implemented for control type '{control_type}'"
-            )
-        return action * params.scale_factor + params.mean
 
     @staticmethod
     @jax.jit
@@ -181,23 +158,15 @@ class CrazyflowBaseEnv(VectorEnv):
         super().reset(seed=seed)
         if seed is not None:
             self.jax_key = jax.random.key(seed)
-        # Resets ALL (!) environments
-        if options is None:
-            options = {}
 
         self.reset(mask=jnp.ones((self.sim.n_worlds), dtype=jnp.bool_))
-
         self.prev_done = jnp.zeros((self.sim.n_worlds), dtype=jnp.bool_)
-
         return self._obs(), {}
 
     def reset(self, mask: Array) -> None:
         self.sim.reset(mask=mask)
-
         mask3d = mask[:, None, None]
-
         # NOTE Setting initial ryp_rate when using physics.sys_id will not have an impact
-
         # Sample initial pos
         self.jax_key, subkey = jax.random.split(self.jax_key)
         init_pos = jax.random.uniform(
@@ -209,7 +178,6 @@ class CrazyflowBaseEnv(VectorEnv):
         self.sim.states = self.sim.states.replace(
             pos=jnp.where(mask3d, init_pos, self.sim.states.pos)
         )
-
         # Sample initial vel
         self.jax_key, subkey = jax.random.split(self.jax_key)
         init_vel = jax.random.uniform(
@@ -246,7 +214,7 @@ class CrazyflowBaseEnv(VectorEnv):
 
     @staticmethod
     @jax.jit
-    def _truncated(dones: Array, time: Array, time_horizon_in_seconds: Array) -> Array:
+    def _truncated(dones: Array, time: Array, time_horizon_in_seconds: float) -> Array:
         truncated = time >= time_horizon_in_seconds
         return jnp.where(dones, False, truncated)
 
@@ -255,12 +223,9 @@ class CrazyflowBaseEnv(VectorEnv):
 
     def _obs(self) -> dict[str, Array]:
         convert = self.return_datatype == "numpy"
-        fields = self.states_to_include_in_obs
+        fields = self.obs_keys
         states = [maybe_to_numpy(getattr(self.sim.states, field), convert) for field in fields]
-        obs = {k: v for k, v in zip(fields, states)}
-        if "pos" in obs:
-            obs["pos"] = obs["pos"][..., 2]
-        return obs
+        return {k: v for k, v in zip(fields, states)}
 
 
 class CrazyflowEnvReachGoal(CrazyflowBaseEnv):
@@ -270,12 +235,11 @@ class CrazyflowEnvReachGoal(CrazyflowBaseEnv):
         assert kwargs["n_drones"] == 1, "Currently only supported for one drone"
 
         super().__init__(**kwargs)
-        self._obs_size += 3  # difference to goal position
-        self.single_observation_space = spaces.Box(
-            -jnp.inf, jnp.inf, shape=(self._obs_size,), dtype=jnp.float32
-        )
+        spec = {k: v for k, v in self.single_observation_space.items()}
+        spec["difference_to_goal"] = spaces.Box(-np.inf, np.inf, shape=(3,))
+        self.single_observation_space = spaces.Dict(spec)
         self.observation_space = batch_space(self.single_observation_space, self.sim.n_worlds)
-        self.goal = jnp.zeros((kwargs["n_worlds"], 3), dtype=jnp.float32, device=self.device)
+        self.goal = jnp.zeros((self.sim.n_worlds, 3), dtype=jnp.float32, device=self.device)
 
     @property
     def reward(self) -> Array:
@@ -313,16 +277,13 @@ class CrazyflowEnvTargetVelocity(CrazyflowBaseEnv):
     """JAX Gymnasium environment for Crazyflie simulation."""
 
     def __init__(self, **kwargs: dict):
-        assert kwargs["n_drones"] == 1, "Currently only supported for one drone"
-
         super().__init__(**kwargs)
-        self._obs_size += 3  # difference to target velocity
-        self.single_observation_space = spaces.Box(
-            -jnp.inf, jnp.inf, shape=(self._obs_size,), dtype=jnp.float32
-        )
+        assert self.sim.n_drones == 1, "Currently only supported for one drone"
+        spec = {k: v for k, v in self.single_observation_space.items()}
+        spec["difference_to_target_vel"] = spaces.Box(-np.inf, np.inf, shape=(3,))
+        self.single_observation_space = spaces.Dict(spec)
         self.observation_space = batch_space(self.single_observation_space, self.sim.n_worlds)
-
-        self.target_vel = jnp.zeros((kwargs["n_worlds"], 3), dtype=jnp.float32)
+        self.target_vel = jnp.zeros((self.sim.n_worlds, 3), dtype=jnp.float32)
 
     @property
     def reward(self) -> Array:

crazyflow/sim/core.py
@@ -19,6 +19,7 @@ from crazyflow.sim.fused import (
     fused_identified_dynamics,
     fused_masked_attitude2rpm,
     fused_masked_state2attitude,
+    fused_masked_thrust2rpm,
     fused_rpms2collective_wrench,
 )
 from crazyflow.sim.physics import Physics
@@ -55,6 +56,8 @@ class Sim:
         # the same time. We raise if the user tries a combination of pycffirmware and drones > 1.
         if controller == Controller.pycffirmware and n_worlds * n_drones != 1:
             raise ConfigError("pycffirmware controller is only supported for single drone sims")
+        if control == Control.thrust and physics != Physics.analytical:
+            raise ConfigError("Thrust control is only supported for analytical physics")
         # Allocate internal states and controls for analytical and sys_id physics.
         self.n_worlds = n_worlds
         self.n_drones = n_drones
@@ -162,7 +165,10 @@ class Sim:
         self.controls = self._state_control(cmd, self.controls, self.device)
 
     def thrust_control(self, cmd: Array):
-        raise NotImplementedError
+        """Set the desired thrust for all drones in all worlds."""
+        assert cmd.shape == (self.n_worlds, self.n_drones, 4), "Command shape mismatch"
+        assert self.control == Control.thrust, "Thrust control is not enabled by the sim config"
+        self.controls = self._thrust_control(cmd, self.controls, self.device)
 
     def render(self):
         if self.viewer is None:
@@ -240,6 +246,8 @@ class Sim:
 
     def _step_emulate_firmware(self) -> SimControls:
         mask = self.controllable
+        if self.control == Control.thrust:
+            return fused_masked_thrust2rpm(mask, self.controls)
         if self.control == Control.state:
             self.controls = self._masked_state_controls_update(mask, self.controls)
             self.controls = fused_masked_state2attitude(mask, self.states, self.controls, self.dt)
@@ -321,6 +329,11 @@ class Sim:
     @partial(jax.jit, static_argnames="device")
     def _state_control(cmd: Array, controls: SimControls, device: str) -> SimControls:
         return controls.replace(staged_state=jnp.array(cmd, device=device))
+
+    @staticmethod
+    @partial(jax.jit, static_argnames="device")
+    def _thrust_control(cmd: Array, controls: SimControls, device: str) -> SimControls:
+        return controls.replace(thrust=jnp.array(cmd, device=device))
 
     @staticmethod
     @jax.jit

crazyflow/sim/fused.py
@@ -11,7 +11,7 @@ import jax.numpy as jnp
 from jax import Array
 from jax.scipy.spatial.transform import Rotation as R
 
-from crazyflow.control.controller import attitude2rpm, state2attitude
+from crazyflow.control.controller import attitude2rpm, pwm2rpm, state2attitude, thrust2pwm
 from crazyflow.sim.physics import analytical_dynamics, identified_dynamics, rpms2collective_wrench
 from crazyflow.sim.structs import SimControls, SimParams, SimState
 
@@ -141,3 +141,17 @@ def fused_rpms2collective_wrench(
     """
     forces, torques = rpms2collective_wrench(cmd.rpms, states.quat, states.rpy_rates, params.J)
     return forces, torques
+
+
+@jax.jit
+@partial(jax.vmap, in_axes=(0, 0))
+@partial(jax.vmap, in_axes=(None, 0))
+def fused_masked_thrust2rpm(mask: Array, cmd: SimControls) -> SimControls:
+    """Convert thrust to RPMs.
+
+    Note:
+        Fused version of `crazyflow.control.controller.thrust2rpm`. See `crazyflow.sim.fused` for
+        more details.
+    """
+    rpms = jnp.where(mask, pwm2rpm(thrust2pwm(cmd.thrust)), cmd.rpms)
+    return cmd.replace(rpms=rpms)