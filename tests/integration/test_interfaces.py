7da06ea6b9b74f19fa4131b7c5dfced24ab7ed59
Martin Schuck <martin.schuck@tum.de>
2024-12-13 22:55:53 +0100

    Fix integration tests. Revert flawed test optimization

--- tests/integration/test_interfaces.py
+++ tests/integration/test_interfaces.py
@@ -8,9 +8,6 @@ from crazyflow.sim.core import Physics, Sim
 @pytest.mark.integration
 @pytest.mark.parametrize("physics", Physics)
 def test_state_interface(physics: Physics):
-    # Create environment with 1 world and 1 drone
-    if physics == Physics.sys_id:
-        pytest.skip("sys_id physics does not support state control")
     sim = Sim(physics=physics, control=Control.state)
 
     # Run simulation for 2 seconds
@@ -20,36 +17,36 @@ def test_state_interface(physics: Physics):
         cmd = cmd.at[0, 0, 2].set(1.0)  # Set z position target to 1.0
         sim.state_control(cmd)
         sim.step()
-        if jnp.linalg.norm(sim.states.pos[0, 0] - jnp.array([0.0, 0.0, 1.0])) < 0.1:
+        if jnp.linalg.norm(sim.data.states.pos[0, 0] - jnp.array([0.0, 0.0, 1.0])) < 0.1:
             break
 
     # Check if drone reached target position
-    distance = jnp.linalg.norm(sim.states.pos[0, 0] - jnp.array([0.0, 0.0, 1.0]))
+    distance = jnp.linalg.norm(sim.data.states.pos[0, 0] - jnp.array([0.0, 0.0, 1.0]))
     assert distance < 0.1, f"Failed to reach target height with {physics} physics"
 
 
 @pytest.mark.integration
 @pytest.mark.parametrize("physics", Physics)
 def test_attitude_interface(physics: Physics):
-    # Create environment with 1 world and 1 drone
     sim = Sim(physics=physics, control=Control.attitude)
     target_pos = jnp.array([0.0, 0.0, 1.0])
 
     i_error = jnp.zeros((1, 1, 3))
 
     for _ in range(int(2 * sim.freq)):  # Run simulation for 2 seconds
-        pos, vel, quat = sim.states.pos, sim.states.vel, sim.states.quat
+        pos, vel, quat = sim.data.states.pos, sim.data.states.vel, sim.data.states.quat
         des_pos = jnp.array([[[0, 0, 1.0]]])
+        dt = 1 / sim.data.core.freq
         cmd, i_error = state2attitude(
-            pos, vel, quat, des_pos, jnp.zeros((1, 1, 3)), jnp.zeros((1, 1, 1)), i_error, sim.dt
+            pos, vel, quat, des_pos, jnp.zeros((1, 1, 3)), jnp.zeros((1, 1, 1)), i_error, dt
         )
         sim.attitude_control(cmd)
         sim.step()
-        if jnp.linalg.norm(sim.states.pos[0, 0] - target_pos) < 0.1:
+        if jnp.linalg.norm(sim.data.states.pos[0, 0] - target_pos) < 0.1:
             break
 
     # Check if drone maintained hover position
-    dpos = sim.states.pos[0, 0] - target_pos
+    dpos = sim.data.states.pos[0, 0] - target_pos
     distance = jnp.linalg.norm(dpos)
     assert distance < 0.1, f"Failed to maintain hover with {physics} ({dpos})"
 
@@ -59,16 +56,16 @@ def test_attitude_interface(physics: Physics):
 def test_swarm_control(physics: Physics):
     n_worlds, n_drones = 2, 3
     sim = Sim(n_worlds=n_worlds, n_drones=n_drones, physics=physics, control=Control.state)
-    target_pos = sim.states.pos + jnp.array([0.2, 0.2, 0.2])
+    target_pos = sim.data.states.pos + jnp.array([0.2, 0.2, 0.2])
 
     for _ in range(int(5 * sim.freq)):  # Run simulation for 2 seconds
         cmd = jnp.zeros((n_worlds, n_drones, 13))
         cmd = cmd.at[..., :3].set(target_pos)
         sim.state_control(cmd)
         sim.step()
-        if jnp.linalg.norm(sim.states.pos[0, 0] - target_pos) < 0.1:
+        if jnp.linalg.norm(sim.data.states.pos[0, 0] - target_pos) < 0.1:
             break
 
     # Check if drone maintained hover position
-    max_dist = jnp.max(jnp.linalg.norm(sim.states.pos - target_pos, axis=-1))
+    max_dist = jnp.max(jnp.linalg.norm(sim.data.states.pos - target_pos, axis=-1))
     assert max_dist < 0.05, f"Failed to reach target, max dist: {max_dist}"
