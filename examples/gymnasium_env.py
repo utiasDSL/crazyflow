f110a400db4d048df0d16df92e60abcb55aabd2d
OliEfr <ollihaus@t-online.de>
2024-12-15 18:17:25 +0100

    add testing for example scripts

--- examples/gymnasium_env.py
+++ examples/gymnasium_env.py
@@ -6,48 +6,47 @@ from ml_collections import config_dict
 from crazyflow.control.controller import Control, Controller
 from crazyflow.sim.physics import Physics
 
-from pyvirtualdisplay import Display
+def main():
+    # set config for simulation
+    sim_config = config_dict.ConfigDict()
+    sim_config.device = "cpu"
+    sim_config.physics = Physics.sys_id
+    sim_config.control = Control.default
+    sim_config.controller = Controller.default
+    sim_config.control_freq = 50
+    sim_config.n_drones = 1
+    sim_config.n_worlds = 20
 
-disp = Display()
-disp.start()
+    SEED = 42
 
-# set config for simulation
-sim_config = config_dict.ConfigDict()
-sim_config.device = "cpu"
-sim_config.physics = Physics.sys_id
-sim_config.control = Control.default
-sim_config.controller = Controller.default
-sim_config.control_freq = 50
-sim_config.n_drones = 1
-sim_config.n_worlds = 20
+    envs = gymnasium.make_vec(
+        "DroneReachPos-v0",
+        time_horizon_in_seconds=2,
+        num_envs=sim_config.n_worlds,
+        **sim_config,
+    )
 
-SEED = 42
+    # RL wrapper
+    # envs = CrazyflowRL(
+    #     envs
+    # )  # This wrapper to clips the actions to [-1, 1], and rescales them subsequently for use with common DRL libraries.
 
-envs = gymnasium.make_vec(
-    "DroneReachPos-v0",
-    time_horizon_in_seconds=2,
-    num_envs=sim_config.n_worlds,
-    **sim_config,
-)
+    envs = JaxToNumpy(
+        envs
+    )  #  This wrapper makes it possible to interact with the environment using numpy arrays, if desired. JaxToTorch is available as well.
 
-# RL wrapper
-# envs = CrazyflowRL(
-#     envs
-# )  # This wrapper to clips the actions to [-1, 1], and rescales them subsequently for use with common DRL libraries.
+    # action for going up (in attitude control)
+    action = np.zeros((sim_config.n_worlds * sim_config.n_drones, 4), dtype=np.float32)
+    action[..., 0] = 0.4
 
-envs = JaxToNumpy(
-    envs
-)  #  This wrapper makes it possible to interact with the environment using numpy arrays, if desired. JaxToTorch is available as well.
+    obs, info = envs.reset(seed=SEED)
 
-# action for going up (in attitude control)
-action = np.zeros((sim_config.n_worlds * sim_config.n_drones, 4), dtype=np.float32)
-action[..., 0] = 0.4
+    # Step through the environment
+    for _ in range(1500):
+        observation, reward, terminated, truncated, info = envs.step(action)
+        # envs.render()
 
-obs, info = envs.reset(seed=SEED)
+    envs.close()
 
-# Step through the environment
-for _ in range(1500):
-    observation, reward, terminated, truncated, info = envs.step(action)
-    envs.render()
-
-envs.close()
+if __name__ == "__main__":
+    main()
