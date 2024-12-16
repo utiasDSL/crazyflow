b0a1788a26b5428ca4f54aba6d59157f737b21ab
OliEfr <ollihaus@t-online.de>
2024-12-16 10:18:41 +0100

    fix style

--- examples/render.py
+++ examples/render.py
@@ -6,7 +6,7 @@ from crazyflow.sim import Physics, Sim
 def main():
     """Spawn 5 drones in one world and render it."""
     n_worlds, n_drones = 1, 100
-    sim = Sim(n_worlds=n_worlds, n_drones=n_drones, physics=Physics.sys_id, device="gpu", freq=60)
+    sim = Sim(n_worlds=n_worlds, n_drones=n_drones, physics=Physics.sys_id, device="cpu", freq=60)
     fps = 60
     cmd = np.array([[[0.3, 0, 0, 0] for _ in range(n_drones)] for _ in range(n_worlds)])
     for _ in range(int(5 * fps)):
