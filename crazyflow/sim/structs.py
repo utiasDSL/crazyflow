ba00efc40040b104b14ae022557b322ce9dad43a
Martin Schuck <martin.schuck@tum.de>
2024-12-29 13:55:47 +0100

    Implement mujoco physics pipeline. Improve performance with mjx_model in pipeline step closure. Fix ang_vel conversion when syncing to MuJoCo. Add mjx_data and mjx_model (optional) to SimData. Fix pytree_replace for non-compatible shapes.

--- crazyflow/sim/structs.py
+++ crazyflow/sim/structs.py
@@ -1,7 +1,14 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 import jax
 import jax.numpy as jnp
 from flax.struct import dataclass, field
 from jax import Array, Device
+
+if TYPE_CHECKING:
+    from mujoco.mjx import Data, Model
 
 
 @dataclass
@@ -15,28 +22,28 @@ class SimState:
     motor_forces: Array  # (N, M, 4)  # Motor forces along body frame z axis
     motor_torques: Array  # (N, M, 4)  # Motor torques around the body frame z axis
 
-
-def default_state(n_worlds: int, n_drones: int, device: Device) -> SimState:
-    """Create a default set of states for the simulation."""
-    pos = jnp.zeros((n_worlds, n_drones, 3), device=device)
-    quat = jnp.zeros((n_worlds, n_drones, 4), device=device)
-    quat = quat.at[..., -1].set(1.0)
-    vel = jnp.zeros((n_worlds, n_drones, 3), device=device)
-    rpy_rates = jnp.zeros((n_worlds, n_drones, 3), device=device)
-    force = jnp.zeros((n_worlds, n_drones, 3), device=device)
-    torque = jnp.zeros((n_worlds, n_drones, 3), device=device)
-    motor_forces = jnp.zeros((n_worlds, n_drones, 4), device=device)
-    motor_torques = jnp.zeros((n_worlds, n_drones, 4), device=device)
-    return SimState(
-        pos=pos,
-        quat=quat,
-        force=force,
-        torque=torque,
-        motor_forces=motor_forces,
-        motor_torques=motor_torques,
-        vel=vel,
-        rpy_rates=rpy_rates,
-    )
+    @staticmethod
+    def create(n_worlds: int, n_drones: int, device: Device) -> SimState:
+        """Create a default set of states for the simulation."""
+        pos = jnp.zeros((n_worlds, n_drones, 3), device=device)
+        quat = jnp.zeros((n_worlds, n_drones, 4), device=device)
+        quat = quat.at[..., -1].set(1.0)
+        vel = jnp.zeros((n_worlds, n_drones, 3), device=device)
+        rpy_rates = jnp.zeros((n_worlds, n_drones, 3), device=device)
+        force = jnp.zeros((n_worlds, n_drones, 3), device=device)
+        torque = jnp.zeros((n_worlds, n_drones, 3), device=device)
+        motor_forces = jnp.zeros((n_worlds, n_drones, 4), device=device)
+        motor_torques = jnp.zeros((n_worlds, n_drones, 4), device=device)
+        return SimState(
+            pos=pos,
+            quat=quat,
+            force=force,
+            torque=torque,
+            motor_forces=motor_forces,
+            motor_torques=motor_torques,
+            vel=vel,
+            rpy_rates=rpy_rates,
+        )
 
 
 @dataclass
@@ -46,14 +53,14 @@ class SimStateDeriv:
     dvel: Array  # (N, M, 3)
     drpy_rates: Array  # (N, M, 3)
 
-
-def default_state_deriv(n_worlds: int, n_drones: int, device: Device) -> SimStateDeriv:
-    """Create a default set of state derivatives for the simulation."""
-    dpos = jnp.zeros((n_worlds, n_drones, 3), device=device)
-    drot = jnp.zeros((n_worlds, n_drones, 3), device=device)
-    dvel = jnp.zeros((n_worlds, n_drones, 3), device=device)
-    drpy_rates = jnp.zeros((n_worlds, n_drones, 3), device=device)
-    return SimStateDeriv(dpos=dpos, drot=drot, dvel=dvel, drpy_rates=drpy_rates)
+    @staticmethod
+    def create(n_worlds: int, n_drones: int, device: Device) -> SimStateDeriv:
+        """Create a default set of state derivatives for the simulation."""
+        dpos = jnp.zeros((n_worlds, n_drones, 3), device=device)
+        drot = jnp.zeros((n_worlds, n_drones, 3), device=device)
+        dvel = jnp.zeros((n_worlds, n_drones, 3), device=device)
+        drpy_rates = jnp.zeros((n_worlds, n_drones, 3), device=device)
+        return SimStateDeriv(dpos=dpos, drot=drot, dvel=dvel, drpy_rates=drpy_rates)
 
 
 @dataclass
@@ -73,33 +80,33 @@ class SimControls:
     pos_err_i: Array  # (N, M, 3)
     last_rpy: Array  # (N, M, 3)
 
-
-def default_controls(
-    n_worlds: int,
-    n_drones: int,
-    state_freq: int = 100,
-    attitude_freq: int = 500,
-    thrust_freq: int = 500,
-    device: Device | str = "cpu",
-) -> SimControls:
-    """Create a default set of controls for the simulation."""
-    device = jax.devices(device)[0] if isinstance(device, str) else device
-    return SimControls(
-        state=jnp.zeros((n_worlds, n_drones, 13), device=device),
-        state_steps=-jnp.ones((n_worlds, 1), dtype=jnp.int32, device=device),
-        state_freq=state_freq,
-        attitude=jnp.zeros((n_worlds, n_drones, 4), device=device),
-        staged_attitude=jnp.zeros((n_worlds, n_drones, 4), device=device),
-        attitude_steps=-jnp.ones((n_worlds, 1), dtype=jnp.int32, device=device),
-        attitude_freq=attitude_freq,
-        thrust=jnp.zeros((n_worlds, n_drones, 4), device=device),
-        thrust_steps=-jnp.ones((n_worlds, 1), dtype=jnp.int32, device=device),
-        thrust_freq=thrust_freq,
-        rpms=jnp.zeros((n_worlds, n_drones, 4), device=device),
-        rpy_err_i=jnp.zeros((n_worlds, n_drones, 3), device=device),
-        pos_err_i=jnp.zeros((n_worlds, n_drones, 3), device=device),
-        last_rpy=jnp.zeros((n_worlds, n_drones, 3), device=device),
-    )
+    @staticmethod
+    def create(
+        n_worlds: int,
+        n_drones: int,
+        state_freq: int = 100,
+        attitude_freq: int = 500,
+        thrust_freq: int = 500,
+        device: Device | str = "cpu",
+    ) -> SimControls:
+        """Create a default set of controls for the simulation."""
+        device = jax.devices(device)[0] if isinstance(device, str) else device
+        return SimControls(
+            state=jnp.zeros((n_worlds, n_drones, 13), device=device),
+            state_steps=-jnp.ones((n_worlds, 1), dtype=jnp.int32, device=device),
+            state_freq=state_freq,
+            attitude=jnp.zeros((n_worlds, n_drones, 4), device=device),
+            staged_attitude=jnp.zeros((n_worlds, n_drones, 4), device=device),
+            attitude_steps=-jnp.ones((n_worlds, 1), dtype=jnp.int32, device=device),
+            attitude_freq=attitude_freq,
+            thrust=jnp.zeros((n_worlds, n_drones, 4), device=device),
+            thrust_steps=-jnp.ones((n_worlds, 1), dtype=jnp.int32, device=device),
+            thrust_freq=thrust_freq,
+            rpms=jnp.zeros((n_worlds, n_drones, 4), device=device),
+            rpy_err_i=jnp.zeros((n_worlds, n_drones, 3), device=device),
+            pos_err_i=jnp.zeros((n_worlds, n_drones, 3), device=device),
+            last_rpy=jnp.zeros((n_worlds, n_drones, 3), device=device),
+        )
 
 
 @dataclass
@@ -108,16 +115,16 @@ class SimParams:
     J: Array  # (N, M, 3, 3)
     J_INV: Array  # (N, M, 3, 3)
 
-
-def default_params(
-    n_worlds: int, n_drones: int, mass: float, J: Array, J_INV: Array, device: Device
-) -> SimParams:
-    """Create a default set of parameters for the simulation."""
-    mass = jnp.ones((n_worlds, n_drones, 1), device=device) * mass
-    j, j_inv = jnp.array(J, device=device), jnp.array(J_INV, device=device)
-    J = jnp.tile(j[None, None, :, :], (n_worlds, n_drones, 1, 1))
-    J_INV = jnp.tile(j_inv[None, None, :, :], (n_worlds, n_drones, 1, 1))
-    return SimParams(mass=mass, J=J, J_INV=J_INV)
+    @staticmethod
+    def create(
+        n_worlds: int, n_drones: int, mass: float, J: Array, J_INV: Array, device: Device
+    ) -> SimParams:
+        """Create a default set of parameters for the simulation."""
+        mass = jnp.ones((n_worlds, n_drones, 1), device=device) * mass
+        j, j_inv = jnp.array(J, device=device), jnp.array(J_INV, device=device)
+        J = jnp.tile(j[None, None, :, :], (n_worlds, n_drones, 1, 1))
+        J_INV = jnp.tile(j_inv[None, None, :, :], (n_worlds, n_drones, 1, 1))
+        return SimParams(mass=mass, J=J, J_INV=J_INV)
 
 
 @dataclass
@@ -126,14 +133,24 @@ class SimCore:
     steps: Array  # (N, 1)
     n_worlds: int = field(pytree_node=False)
     n_drones: int = field(pytree_node=False)
+    drone_ids: Array  # (1, M)
     rng_key: Array  # (N, 1)
 
-
-def default_core(freq: int, n_worlds: int, n_drones: int, rng_key: int, device: Device) -> SimCore:
-    """Create a default set of core simulation parameters."""
-    steps = jnp.zeros((n_worlds, 1), dtype=jnp.int32, device=device)
-    rng_key = jax.device_put(jax.random.key(rng_key), device)
-    return SimCore(freq=freq, steps=steps, n_worlds=n_worlds, n_drones=n_drones, rng_key=rng_key)
+    @staticmethod
+    def create(
+        freq: int, n_worlds: int, n_drones: int, drone_ids: Array, rng_key: int, device: Device
+    ) -> SimCore:
+        """Create a default set of core simulation parameters."""
+        steps = jnp.zeros((n_worlds, 1), dtype=jnp.int32, device=device)
+        rng_key = jax.device_put(jax.random.key(rng_key), device)
+        return SimCore(
+            freq=freq,
+            steps=steps,
+            n_worlds=n_worlds,
+            n_drones=n_drones,
+            drone_ids=jnp.array(drone_ids, dtype=jnp.int32, device=device),
+            rng_key=rng_key,
+        )
 
 
 @dataclass
@@ -143,3 +160,5 @@ class SimData:
     controls: SimControls
     params: SimParams
     core: SimCore
+    mjx_data: Data
+    mjx_model: Model | None
--- examples/disturbance.py
+++ examples/disturbance.py
@@ -14,7 +14,7 @@ def disturbance_fn(data: SimData) -> SimData:
     states = states.replace(force=states.force + disturbance_force)
 
     key, subkey = jax.random.split(key)
-    disturbance_torque = jax.random.normal(subkey, states.torque.shape) * 2e-3
+    disturbance_torque = jax.random.normal(subkey, states.torque.shape) * 2e-4
     states = states.replace(torque=states.torque + disturbance_torque)
 
     return data.replace(states=states, core=data.core.replace(rng_key=key))
@@ -23,61 +23,59 @@ def disturbance_fn(data: SimData) -> SimData:
 def main(plot: bool = False):
     sim = Sim(control="state")
     control = np.zeros((sim.n_worlds, sim.n_drones, 13))
-    control[..., :3] = 0.4
+    control[..., :3] = 0.2
 
     # First run
-    pos_unperturbed = []
-    rpy_unperturbed = []
+    pos, rpy = [], []
     sim.reset()
     for _ in range(3 * sim.control_freq):
         sim.state_control(control)
         sim.step(sim.freq // sim.control_freq)
-        pos_unperturbed.append(sim.data.states.pos[0, 0])
-        rpy_unperturbed.append(sim.data.states.rpy_rates[0, 0])
+        pos.append(sim.data.states.pos[0, 0])
+        rpy.append(sim.data.states.rpy_rates[0, 0])
         sim.render()
 
     # Second run
     sim.disturbance_fn = disturbance_fn
     sim.build()
-    pos_perturbed = []
-    rpy_perturbed = []
+    pos_disturbed, rpy_disturbed = [], []
     sim.reset()
     for _ in range(3 * sim.control_freq):
         sim.state_control(control)
         sim.step(sim.freq // sim.control_freq)
-        pos_perturbed.append(sim.data.states.pos[0, 0])
-        rpy_perturbed.append(sim.data.states.rpy_rates[0, 0])
+        pos_disturbed.append(sim.data.states.pos[0, 0])
+        rpy_disturbed.append(sim.data.states.rpy_rates[0, 0])
         sim.render()
 
     sim.close()
     if plot:
-        plot_results(pos_unperturbed, pos_perturbed, rpy_unperturbed, rpy_perturbed)
+        plot_results(pos, pos_disturbed, rpy, rpy_disturbed)
 
 
 def plot_results(
-    pos_unperturbed: list[NDArray],
-    pos_perturbed: list[NDArray],
-    rpy_unperturbed: list[NDArray],
-    rpy_perturbed: list[NDArray],
+    pos: list[NDArray],
+    pos_disturbed: list[NDArray],
+    rpy: list[NDArray],
+    rpy_disturbed: list[NDArray],
 ):
-    pos_unperturbed, pos_perturbed = np.array(pos_unperturbed), np.array(pos_perturbed)
-    rpy_unperturbed, rpy_perturbed = np.array(rpy_unperturbed), np.array(rpy_perturbed)
+    pos, pos_disturbed = np.array(pos), np.array(pos_disturbed)
+    rpy, rpy_disturbed = np.array(rpy), np.array(rpy_disturbed)
     fig, ax = plt.subplots(3, 2)
-    t = np.linspace(0, 3, len(pos_unperturbed))
+    t = np.linspace(0, 3, len(pos))
     # XYZ position
-    ax[0, 0].plot(t, pos_unperturbed[:, 0], label="x unperturbed", color="r")
-    ax[0, 0].plot(t, pos_perturbed[:, 0], label="x perturbed", color="r", linestyle="--")
-    ax[1, 0].plot(t, pos_unperturbed[:, 1], label="y unperturbed", color="g")
-    ax[1, 0].plot(t, pos_perturbed[:, 1], label="y perturbed", color="g", linestyle="--")
-    ax[2, 0].plot(t, pos_unperturbed[:, 2], label="z unperturbed", color="b")
-    ax[2, 0].plot(t, pos_perturbed[:, 2], label="z perturbed", color="b", linestyle="--")
+    ax[0, 0].plot(t, pos[:, 0], label="x undisturbed", color="r")
+    ax[0, 0].plot(t, pos_disturbed[:, 0], label="x disturbed", color="r", linestyle="--")
+    ax[1, 0].plot(t, pos[:, 1], label="y undisturbed", color="g")
+    ax[1, 0].plot(t, pos_disturbed[:, 1], label="y perturbed", color="g", linestyle="--")
+    ax[2, 0].plot(t, pos[:, 2], label="z undisturbed", color="b")
+    ax[2, 0].plot(t, pos_disturbed[:, 2], label="z disturbed", color="b", linestyle="--")
     # RPY rates
-    ax[0, 1].plot(t, rpy_unperturbed[:, 0], label="roll unperturbed", color="r")
-    ax[0, 1].plot(t, rpy_perturbed[:, 0], label="roll perturbed", color="r", linestyle="--")
-    ax[1, 1].plot(t, rpy_unperturbed[:, 1], label="pitch unperturbed", color="g")
-    ax[1, 1].plot(t, rpy_perturbed[:, 1], label="pitch perturbed", color="g", linestyle="--")
-    ax[2, 1].plot(t, rpy_unperturbed[:, 2], label="yaw unperturbed", color="b")
-    ax[2, 1].plot(t, rpy_perturbed[:, 2], label="yaw perturbed", color="b", linestyle="--")
+    ax[0, 1].plot(t, rpy[:, 0], label="roll undisturbed", color="r")
+    ax[0, 1].plot(t, rpy_disturbed[:, 0], label="roll disturbed", color="r", linestyle="--")
+    ax[1, 1].plot(t, rpy[:, 1], label="pitch undisturbed", color="g")
+    ax[1, 1].plot(t, rpy_disturbed[:, 1], label="pitch disturbed", color="g", linestyle="--")
+    ax[2, 1].plot(t, rpy[:, 2], label="yaw undisturbed", color="b")
+    ax[2, 1].plot(t, rpy_disturbed[:, 2], label="yaw disturbed", color="b", linestyle="--")
     fig.suptitle("Dynamics with disturbance")
     fig.supxlabel("Time (s)")
     ax[0, 0].sharex(ax[1, 0])
