ec3a8d2a45e6cc1e05e6adc8aa72b2a1a284c527
Martin Schuck <martin.schuck@tum.de>
2025-06-26 12:35:45 +0200

    Fix Crazyflie mass

--- crazyflow/constants.py
+++ crazyflow/constants.py
@@ -8,6 +8,7 @@ GRAVITY: float = 9.81
 ARM_LEN: float = 0.0325 * np.sqrt(2)
 MIX_MATRIX: NDArray = np.array([[-0.5, -0.5, -1], [-0.5, 0.5, 1], [0.5, 0.5, -1], [0.5, -0.5, 1]])
 SIGN_MIX_MATRIX: NDArray = np.sign(MIX_MATRIX)
-MASS: float = 0.027
+# Crazyflie 2.1 mass as measured in the lab with battery included
+MASS: float = 0.033
 J: NDArray = np.array([[2.3951e-5, 0, 0], [0, 2.3951e-5, 0], [0, 0, 3.2347e-5]])
 J_INV: NDArray = np.linalg.inv(J)
