Add temporary gradient fix

crazyflow/sim/physics.py
@@ -86,7 +86,10 @@ def collective_force2acceleration(force: Array, mass: Array) -> Array:
 @partial(vectorize, signature="(3),(4),(3,3)->(3)")
 def collective_torque2ang_vel_deriv(torque: Array, quat: Array, J_inv: Array) -> Array:
     """Convert torques to ang_vel_deriv."""
-    return J_inv @ R.from_quat(quat).apply(torque, inverse=True)
+    # Add a minimal epsilon to avoid breaking gradients when the torques are 0.
+    # TODO: Find out why 0 torques break differentiability and remove epsilon.
+    eps = jnp.minimum(jnp.finfo(torque.dtype).eps, 1e-10)
+    return J_inv @ R.from_quat(quat).apply(torque + eps, inverse=True)
 
 
 @partial(vectorize, signature="(4),(4),(3),(3,3)->(3),(3)")

crazyflow/sim/sim.py
@@ -545,7 +545,7 @@ def step_attitude_controller(data: SimData) -> SimData:
     force = r.apply(jnp.zeros_like(torque).at[..., 2].set(force[..., 0]))
     states = leaf_replace(states, mask, force=force, torque=torque)
     return data.replace(
-        controls=data.controls.replace(attitude=attitude_ctrl, force_torque=ft_ctrl)
+        states=states, controls=data.controls.replace(attitude=attitude_ctrl, force_torque=ft_ctrl)
     )
 
 