175bcc1d4cc034d7aa212a7ec487fa9514ed76b9
Rather1337 <marcel.rath@gmx.de>
2026-03-18 21:22:06 +0100

    Fix render tests

--- tests/unit/test_render.py
+++ tests/unit/test_render.py
@@ -7,8 +7,9 @@ from crazyflow import Sim
 
 @pytest.mark.unit
 @pytest.mark.parametrize("cam_name", ["fpv_cam:0", "track_cam:0", "fpv_cam:1", "track_cam:1"])
+@pytest.mark.render
 @skip_if_headless
-def test_render_camera_selection_from_name(cam_name: str | int):
+def test_render_camera_selection_from_name(cam_name: str):
     sim = Sim(drone_model="cf21B_500", n_drones=2)
     cam_id = mujoco.mj_name2id(
         sim.mj_model, 
@@ -21,8 +22,10 @@ def test_render_camera_selection_from_name(cam_name: str | int):
     assert viewer_cam.fixedcamid == cam_id, f"Expected cam ID {cam_id}, got {viewer_cam.fixedcamid}"
     sim.close()
 
+
 @pytest.mark.unit
 @pytest.mark.parametrize("cam_id", [0, 1, 2, 3])
+@pytest.mark.render
 @skip_if_headless
 def test_render_camera_selection_from_id(cam_id: int):
     sim = Sim(drone_model="cf21B_500", n_drones=2)
@@ -32,7 +35,9 @@ def test_render_camera_selection_from_id(cam_id: int):
     assert viewer_cam.fixedcamid == cam_id, f"Expected cam ID {cam_id}, got {viewer_cam.fixedcamid}"
     sim.close()
 
+
 @pytest.mark.unit
+@pytest.mark.render
 @skip_if_headless
 def test_render_free_camera():
     sim = Sim(drone_model="cf21B_500", n_drones=2)
